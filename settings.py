a301b5d081af55f9604af117beb1a06261d0fe3c
foxxyz <foxxyz@gmail.com>
2014-06-24 00:03:08 -0700

    Missing settings!

---
+++ settings.py
@@ -0,0 +1,11 @@
+# Some additional settings for BookieBot
+
+# Points awarded for guessing exactly or being closest
+CLOSEST_GUESS_POINTS = 1
+EXACT_GUESS_POINTS = 2
+
+# Time after match start after which a match will not be considered "upcoming"
+NEW_MATCH_TIME_OFFSET = 1800 # 30 Minutes
+
+# How soon bets can be placed before match start
+PRE_MATCH_BETTING_TIME = 900 # 15 Minutes
